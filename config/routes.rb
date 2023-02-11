b0d283f531d6a2491c279e25463e1f24159920ff
elvis-george <112096262+elvis-george@users.noreply.github.com>
2023-02-11 15:05:54 -0600

    fixed rootes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :show_hosts
   resources :calendars
 
-  root'show_hosts#index'
+  #root'show_hosts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
