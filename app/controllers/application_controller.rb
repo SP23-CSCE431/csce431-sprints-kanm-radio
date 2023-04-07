e1c10e8213784f81220fe42f5b9fa34a43f8361d
justin-han <JUSTINHAN07@GMAIL.COM>
2023-04-07 09:57:18 -0500

    Add brakeman and update rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-     helper_method :userSignedIn?
+     helper_method :user_signed_in?
 
-     def userSignedIn?
+     def user_signed_in?
           current_user.present?
      end
 end
